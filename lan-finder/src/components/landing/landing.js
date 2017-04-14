7de7b70d7c33682bf7441acb6adb8196f446ec99
Michael Gaudet <Michael.G224@gmail.com>
2017-04-14 17:01:06 -0500

    added placeholder images for maps & styling for landing page

--- lan-finder/src/components/landing/landing.js
+++ lan-finder/src/components/landing/landing.js
@@ -1,35 +1,38 @@
 import React, { Component } from 'react'
-
 import { Link } from 'react-router-dom'
+
+import GoogleMap from '../global/map/map'
+import News from './news/news'
+
 import './landing.css'
 
-export default class Landing extends Component{
-    render(){
-        return(
-            <div>
-                <div className="row">
-                    <div className="col-sm-12">
-                        <div className="card landingImage">
-                            <h1 className=''>stuff</h1>
-                            <br/>
-                            <br/>
-                            <br/>
-                        </div>
-                    </div>
-                    <div className="container">
-                        <div className="row">
-                            <div className="col s12">
-                                <h1>News:</h1>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="col s12">
-                                <h1>Media:</h1>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+export default class Landing extends Component {
+	render( ) {
+		return (
+			<div>
+				<div className="container">
+					<div className="row">
+						<div className="col s12">
+							<div className="card">
+                                <h4 className='center-align'>Events in your area!</h4>
+								{/*set map up to get users location and events in the area*/}
+								<GoogleMap/>
+							</div>
+						</div>
+						<div className="row">
+							<div className="col s12">
+								{/*Fix News Component to dynyamically render news from database*/}
+								<News/>
+							</div>
+						</div>
+						<div className="row">
+							<div className="col s12">
+								<h1>Media:</h1>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		)
+	}
 }
---
+++ lan-finder/src/components/landing/news/news.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export default function News( ) {
+	return (
+		<div className="">
+			<div className="col s12 card">
+                <h4>#Recent News</h4>
+				<div className="card horizontal">
+					<div className="card-image">
+						<img src="http://lorempixel.com/100/190/nature/6"></img>
+					</div>
+					<div className="card-stacked">
+						<div className="card-content">
+							<p className='flow-text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, minima, minus? Officia, magni, alias repellendus beatae voluptatibus numquam! Inventore blanditiis ipsam, quae culpa dolores dicta voluptatem quos placeat magni perferendis!</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+)
+}
