e3a071ef2d8ddbae3c6dfdb2799315d52c8ec09c
Michael Gaudet <Michael.G224@gmail.com>
2017-04-17 12:02:07 -0500

    set up passport through facebook

--- lan-finder/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports={
-    sessionSecret:'JeJoMi',
-    FBSecret:'c4d7be6ed75a32b8ddde5671e0616c67',
-    FBAppId:'632001203676722'
-
-}
---
+++ lan-finder/routes/getDataRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const getDataCtrl = require('../logic/getDataCtrl');
+const router = express.Router()
+
+router.get('/user', getDataCtrl.user)
+
+
+
+module.exports=router
