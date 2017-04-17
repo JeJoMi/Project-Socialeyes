aaf890e1e3035a89e66b9d35820abbc23d1a787b
Michael Gaudet <Michael.G224@gmail.com>
2017-04-16 20:43:06 -0500

    hooked up passport for facebook

--- lan-finder/src/components/landing/userInteraction/loginSignup.js
+++ lan-finder/src/components/landing/userInteraction/loginSignup.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react'
-
+import {Link} from 'react-router-dom'
 export default class UserInteractionCard extends Component {
 	render( ) {
 		return (
 			<div className="card">
-				<div className="card-content red darken-2">
-					<p className='white-text flow-text'>Login or Sign Up. Don't worry it's free
+				<div className="card-content red accent-4">
+					<p className='white-text flow-text'>
+						Login or Sign Up. Don't worry it's free
 					</p>
 				</div>
 				<div className="card-content grey lighten-4">
-				    <button className="btn btn-block blue darken-3">login with Facebook</button>
+					<a href='/auth/facebook' className='btn btn-block blue darken-4'>
+						login with Facebook
+					</a>
 				</div>
 			</div>
 		)
