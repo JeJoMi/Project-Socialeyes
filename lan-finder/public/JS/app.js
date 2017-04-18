4c4e89ddf3537ff61c73fa975452c678aa4b169c
Michael Gaudet <Michael.G224@gmail.com>
2017-04-18 10:18:24 -0500

    set up angular controllers and services

---
+++ lan-finder/public/JS/app.js
@@ -0,0 +1,11 @@
+const app = angular.module('lan-finder', ['ui.router'])
+
+app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
+    $urlRouterProvider.otherwise('/')
+
+    $stateProvider
+    .state('landing', {
+        url:'/',
+        templateUrl:'../views/landing/landingTmpl.html'
+    })
+}])
---
+++ lan-finder/public/JS/services/mainSrvc.js
@@ -0,0 +1,3 @@
+app.service('mainSrvc', ['$http', function($http){
+    
+}])
