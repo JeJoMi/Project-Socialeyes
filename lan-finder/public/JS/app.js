b5d546fb7c3e5b5821c0ff565eaf9be0d90852d6
jmitchell0528 <jlm0528@yahoo.com>
2017-04-18 11:32:29 -0500

    Fixed bug

--- lan-finder/public/JS/app.js
+++ lan-finder/public/JS/app.js
@@ -1,16 +1,5 @@
 const app = angular.module('lan-finder', ['ui.router'])
 
-<<<<<<< HEAD
-// app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
-//     $urlRouterProvider.otherwise('/')
-//
-//     $stateProvider
-//     .state('landing', {
-//         url:'/',
-//         templateUrl:'/views/landing/landingTmpl.html'
-//     })
-// }])
-=======
 app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
     $urlRouterProvider.otherwise('/')
 
@@ -20,4 +9,3 @@ app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $ur
         templateUrl:'../views/landing.html'
     })
 }])
->>>>>>> 2663cf7043118b0a6fef166b8a69333ae38a1463
