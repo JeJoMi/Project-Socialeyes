9a63c1c807bb37c7bd198e5652a68bff6590f423
Michael Gaudet <Michael.G224@gmail.com>
2017-04-18 16:30:58 -0500

    set up the edit profile page

--- lan-finder/server.js
+++ lan-finder/server.js
@@ -58,7 +58,7 @@ passport.deserializeUser(function(obj, done) {
 app.get('/auth/facebook',passport.authenticate('facebook',{scope:['email']}))
 
 app.get('/auth/facebook/callback',
-  passport.authenticate('facebook', { successRedirect: '/getDataFrom/user',
+  passport.authenticate('facebook', { successRedirect: '/#!/home',
                                       failureRedirect: '/' }));
 // ##################################################
 // #################################################
