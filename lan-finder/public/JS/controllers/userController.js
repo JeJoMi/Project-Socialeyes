ceeacf1e48429d6082846efc51b1243d43490983
Michael Gaudet <Michael.G224@gmail.com>
2017-04-19 15:46:27 -0500

    set up s3 file upload for user photos

--- lan-finder/public/JS/controllers/userController.js
+++ lan-finder/public/JS/controllers/userController.js
@@ -1,19 +1,31 @@
-app.controller('userController', ['$scope','userService', function($scope, userService){
-    // FIXME: Add ability to add photos to this
-    $scope.submitProfile = function(firstName, lastName, interests, gender, eventTypes){
-        let eventTypesArr=[]
-        for(let event in eventTypes){
-            if(eventTypes[event]){
-                eventTypesArr.push(event)
-            }
-        }
-        let obj = {
-            firstName, lastName, interests, eventTypes:eventTypesArr, gender
-        }
-        userService.submitProfile(obj).then(x=>{
-          console.log(x);
-        },err=>{
-          console.log(err);
-        })
-    }
-}])
+app.controller('userController', [
+	'$scope',
+	'userService',
+	function ( $scope, userService ) {
+		// NOTE: will handle user uploaded photos through amazon s3
+		$scope.upload = function ( id ) {
+			userService.upload( id )
+		}
+
+		// FIXME: Add ability to add photos to this
+		$scope.submitProfile = function ( firstName, lastName, interests, gender, eventTypes ) {
+			let eventTypesArr = [ ]
+			for ( let event in eventTypes ) {
+				if (eventTypes[event])
+					eventTypesArr.push( event )
+			}
+			let obj = {
+				firstName,
+				lastName,
+				interests,
+				eventTypes: eventTypesArr,
+				gender
+			}
+			userService.submitProfile( obj ).then(x => {
+				console.log( x );
+			}, err => {
+				console.log( err );
+			})
+		}
+	}
+])
