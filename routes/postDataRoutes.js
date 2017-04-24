6f59606d324566689dc38958a87f945723e65163
jonathanrcrook <jonathanrcrook@gmail.com>
2017-04-24 16:41:02 -0500

    can now create events and save to database

--- routes/postDataRoutes.js
+++ routes/postDataRoutes.js
@@ -3,7 +3,7 @@ const postDataCtrl = require('../logic/postDataCtrl.js');
 const router = express.Router();
 
 router.patch('/edit_profile', postDataCtrl.updateProfile);
-// router.post('/events', postDataCtrl.createEvent);
+router.post('/events', postDataCtrl.createEvent);
 
 
 module.exports = router;
