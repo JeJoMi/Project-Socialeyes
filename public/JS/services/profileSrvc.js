7c1314695bbb37bcd091351b44256fd5d0feab9e
Michael Gaudet <Michael.G224@gmail.com>
2017-04-25 18:15:16 -0500

    removed parts of the home page for later use

--- public/JS/services/profileSrvc.js
+++ public/JS/services/profileSrvc.js
@@ -2,4 +2,7 @@ app.service('profileSrvc', ['$http',function($http){
     this.getProfile = function(id){
         return $http.get('/getDataFrom/profile/'+id)
     }
+    this.addFriend = function(obj){
+        return $http.post('/postdatato/')
+    }
 }])
