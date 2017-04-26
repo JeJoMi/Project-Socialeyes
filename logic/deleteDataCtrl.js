8bedb72a67f860c98c6bf56990e24dcc882002e4
jonathanrcrook <jonathanrcrook@gmail.com>
2017-04-26 14:54:16 -0500

    added deleteFriends function

--- logic/deleteDataCtrl.js
+++ logic/deleteDataCtrl.js
@@ -5,5 +5,14 @@ module.exports={
         db.deletePendingFriends([req.query.inviter, req.query.invited], function(err, succ){
             console.log(err, succ);
         })
+    },
+
+    deleteFriends: function(req, res, next) {
+      db.deleteFriends([req.query.user1, req.query.user2], function (err, deletedFriend) {
+        console.log(err, deletedFriend);
+      })
+      db.deleteFriends([req.query.user2, req.query.user3], function (err, deletedFriend) {
+        console.log(err, deletedFriend);
+      })
     }
 }
